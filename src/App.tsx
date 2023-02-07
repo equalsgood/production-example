4aa8581c04e5c6d9a22585f77bd44357c345874a
equalsgood <connection.babak@gmail.com>
2023-02-07 22:45:04 +0200

    two basic themes init, creating context and related to them hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense } from 'react';
-import './index.scss';
+import React, {Suspense, useContext, useState} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
