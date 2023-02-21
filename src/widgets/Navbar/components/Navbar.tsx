cd577ce7f0f25eb1c0330e2fa9e0a23a7404d58b
equalsgood <connection.babak@gmail.com>
2023-02-21 17:48:36 +0200

    translation linter problem fix

--- src/widgets/Navbar/components/Navbar.tsx
+++ src/widgets/Navbar/components/Navbar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { AppLink, AppLinkTheme } from 'shared/components/AppLink/AppLink';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <nav className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/">Main</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">About</AppLink>
-        </div>
-    </nav>
-);
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <nav className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Main')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('About')}</AppLink>
+            </div>
+        </nav>
+    );
+};
--- src/widgets/Sidebar/components/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/components/Sidebar/Sidebar.tsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/components/Button/Button';
 import cls from './Sidebar.module.scss';
-import { Button } from '../../../../shared/components/Button/Button';
 
 interface SidebarProps {
     className?: string;
