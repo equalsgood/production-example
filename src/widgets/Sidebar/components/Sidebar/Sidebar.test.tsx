f4b788a589203a819a70f893e62f050ef66d20db
equalsgood <connection.babak@gmail.com>
2023-02-21 12:32:18 +0200

    adding screenshot tests for visual regressions

--- src/widgets/Sidebar/components/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/components/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from './Sidebar';
 
