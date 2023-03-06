e762fc2cb6c6dc2d8f7cad5b5870be73c6104324
equalsgood <connection.babak@gmail.com>
2023-03-06 13:43:29 +0200

    tests for authByUsername (async thunk test is included)

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,17 @@
+import { loginActions, LoginSchema } from 'features/AuthByUserName';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('12345')))
+            .toStrictEqual({ username: '12345' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('12345')))
+            .toStrictEqual({ password: '12345' });
+    });
+});
